6efb0ed33cbee86c69faef4ac2168288c0098f2d
Steven Wanderski <steven.wanderski@gmail.com>
2025-10-24 11:38:02 -0500

    Fixes more quote consistency

--- spec/features/successful_forecast_spec.rb
+++ spec/features/successful_forecast_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe 'successful forecast', type: :feature do
+describe "successful forecast", type: :feature do
   before do
     allow(WeatherClient).to receive(:fetch).and_return(double(
       forecast: { temp_current: 51 },
@@ -8,11 +8,11 @@ describe 'successful forecast', type: :feature do
     ))
   end
 
-  it 'shows the forecast' do
-    visit '/'
-    fill_in 'Postal', with: '60463'
-    click_button 'Submit'
+  it "shows the forecast" do
+    visit "/"
+    fill_in "Postal", with: "60463"
+    click_button "Submit"
 
-    expect(page).to have_content('The current temperature for 60463 is 51°.')
+    expect(page).to have_content("The current temperature for 60463 is 51°.")
   end
 end
