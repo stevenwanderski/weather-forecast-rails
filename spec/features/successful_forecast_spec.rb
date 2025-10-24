5d6535367ec3c26721b15d46c3679d8c4789f531
Steven Wanderski <steven.wanderski@gmail.com>
2025-10-24 09:12:23 -0500

    Adds homepage feature spec

---
+++ spec/features/successful_forecast_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'successful forecast', type: :feature do
+  before do
+    allow(WeatherClient).to receive(:fetch).and_return({
+      forecast: { temp_current: 51 },
+      success: true
+    })
+  end
+
+  it 'shows the forecast' do
+    visit '/'
+    fill_in 'Postal', with: '60463'
+    click_button 'Submit'
+
+    expect(page).to have_content('The current temperature for 60463 is 51°.')
+  end
+end
